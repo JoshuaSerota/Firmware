PX4/PX4-Autopilot

include remove extra semicolons

src/include/containers/List.hpp
@@ -43,18 +43,16 @@ template<class T>
 class __EXPORT ListNode
 {
 public:
-	ListNode() : _sibling(nullptr)
-	{
-	}
-	virtual ~ListNode() {};
+	ListNode() : _sibling(nullptr) {}
+	virtual ~ListNode() {}
+
 	void setSibling(T sibling) { _sibling = sibling; }
 	T getSibling() { return _sibling; }
-	T get()
-	{
-		return _sibling;
-	}
+	T get() { return _sibling; }
+
 protected:
 	T _sibling;
+
 private:
 	// forbid copy
 	ListNode(const ListNode &other);
@@ -66,19 +64,21 @@ template<class T>
 class __EXPORT List
 {
 public:
-	List() : _head()
-	{
-	}
-	virtual ~List() {};
+	List() : _head() {}
+	virtual ~List() {}
+
 	void add(T newNode)
 	{
 		newNode->setSibling(getHead());
 		setHead(newNode);
 	}
+
 	T getHead() { return _head; }
+
 protected:
 	void setHead(T &head) { _head = head; }
 	T _head;
+
 private:
 	// forbid copy
 	List(const List &other);

JoshuaSerota/Firmware

src/include/containers/List.hpp
@@ -53,7 +53,7 @@ public:
 	ListNode &operator=(ListNode &&) = delete;
 
 	void setSibling(T sibling) { _sibling = sibling; }
-	const T getSibling() { return _sibling; }
+	T getSibling() { return _sibling; }
 
 protected:
 	T _sibling;
@@ -78,7 +78,7 @@ public:
 		_head = newNode;
 	}
 
-	const T getHead() { return _head; }
+	T getHead() { return _head; }
 
 protected:
 	T _head;